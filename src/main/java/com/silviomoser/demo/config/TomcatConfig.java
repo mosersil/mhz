b09cb1e30fafabf8edbcd28e2e5dee01ff8997f4
mosersil <silviomos@gmail.com>
2018-10-30 14:25:52 +0100

    use port 443

--- src/main/java/com/silviomoser/demo/config/TomcatConfig.java
+++ src/main/java/com/silviomoser/demo/config/TomcatConfig.java
@@ -1,17 +1,24 @@
 package com.silviomoser.demo.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Getter
+@Setter
 public class TomcatConfig {
 
+    @Value("${port.https-redirect}")
+    private String portHttps;
 
     @Bean
     public ServletWebServerFactory servletContainer() {
@@ -35,7 +42,7 @@ public class TomcatConfig {
         connector.setScheme("http");
         connector.setPort(8085);
         connector.setSecure(false);
-        connector.setRedirectPort(8443);
+        connector.setRedirectPort(Integer.parseInt(portHttps));
         return connector;
     }
 }
