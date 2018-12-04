6e6d2336d8dd52371aa12707c96b4387f7cad150
mosersil <silviomos@gmail.com>
2018-12-05 00:01:44 +0100

    fix

--- src/main/java/com/silviomoser/demo/data/Membership.java
+++ src/main/java/com/silviomoser/demo/data/Membership.java
@@ -18,6 +18,8 @@ import java.time.LocalDateTime;
 @Table(name = "MEMBERSHIP")
 public class Membership extends AbstractEntity {
 
+
+    @ToString.Exclude
     @ManyToOne
     @JoinColumn(name = "PERSON_ID")
     private Person person;
