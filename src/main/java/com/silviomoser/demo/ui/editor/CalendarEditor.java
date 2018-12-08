80f0939855aaf644242b337814d42ee30e947b36
Silvio <silviomos@gmail.com>
2018-12-08 10:41:19 +0100

    assignable downloads per calendar event

--- src/main/java/com/silviomoser/demo/ui/editor/CalendarEditor.java
+++ src/main/java/com/silviomoser/demo/ui/editor/CalendarEditor.java
@@ -1,7 +1,9 @@
 package com.silviomoser.demo.ui.editor;
 
 import com.silviomoser.demo.data.CalendarEvent;
+import com.silviomoser.demo.data.StaticFile;
 import com.silviomoser.demo.data.type.DressCode;
+import com.silviomoser.demo.repository.StaticFileRepository;
 import com.silviomoser.demo.ui.i18.I18Helper;
 import com.vaadin.data.Binder;
 import com.vaadin.data.provider.DataProvider;
@@ -9,26 +11,32 @@ import com.vaadin.data.validator.StringLengthValidator;
 import com.vaadin.server.VaadinSession;
 import com.vaadin.spring.annotation.SpringComponent;
 import com.vaadin.spring.annotation.UIScope;
-import com.vaadin.ui.CheckBox;
-import com.vaadin.ui.DateTimeField;
-import com.vaadin.ui.FormLayout;
-import com.vaadin.ui.Layout;
-import com.vaadin.ui.RadioButtonGroup;
-import com.vaadin.ui.TextArea;
-import com.vaadin.ui.TextField;
+import com.vaadin.ui.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringComponent
 @UIScope
 public class CalendarEditor extends AbstractEditor<CalendarEvent> {
 
+    @Autowired
+    private StaticFileRepository staticFileRepository;
+
     private I18Helper i18Helper = new I18Helper(VaadinSession.getCurrent().getLocale());
 
     TextField title = new TextField("Anlass");
-    DateTimeField date = new DateTimeField("Datum/Zeit");
+    DateTimeField dateStart = new DateTimeField("Datum/Zeit (Beginn)");
+    DateTimeField dateEnd = new DateTimeField("Datum/Zeit (Beginn)");
     CheckBox fullDay = new CheckBox("Ganztägiger Anlass");
     CheckBox publicEvent = new CheckBox("Öffentlicher Anlass");
     TextArea remarks = new TextArea("Bemerkungen");
     RadioButtonGroup<DressCode> dressCodeRadioButtonGroup = new RadioButtonGroup<>("Dresscode", DataProvider.ofItems(DressCode.values()));
+
+    private Set<StaticFile> files = new HashSet<>();
+    TwinColSelect<StaticFile> filesSelect;
 
 
     @Override
@@ -44,7 +52,17 @@ public class CalendarEditor extends AbstractEditor<CalendarEvent> {
         fullDay.setDescription("Ein ganztägiger Anlass erscheint ohne genaue von/bis Zeitangabe im Veranstaltungskalender");
         publicEvent.setDescription("Ein öffentlicher Anlass erscheint im Veranstaltungskalender auf der Homepage");
 
-        layout.addComponents(title, date, fullDay, publicEvent, remarks, dressCodeRadioButtonGroup);
+
+        filesSelect = new TwinColSelect<>("Downloads", staticFileRepository.findAll());
+        filesSelect.setRows(6);
+        filesSelect.setLeftColumnCaption("Vefügbare Downloads");
+        filesSelect.setRightColumnCaption("Diesem Event zugewiesene Downloads");
+
+
+        // Set full width
+        filesSelect.setWidth(100.0f, Unit.PERCENTAGE);
+
+        layout.addComponents(title, dateStart, dateEnd, fullDay, publicEvent, remarks, dressCodeRadioButtonGroup, filesSelect);
         return layout;
     }
 
@@ -59,6 +77,7 @@ public class CalendarEditor extends AbstractEditor<CalendarEvent> {
                 .asRequired("Bitte Dresscode wählen")
                 .bind(CalendarEvent::getDressCode, CalendarEvent::setDressCode);
 
+        binder.forField(filesSelect).bind(CalendarEvent::getFiles, CalendarEvent::setFiles);
         binder.bindInstanceFields(this);
         return binder;
     }
