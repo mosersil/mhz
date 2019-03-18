3ddfc0148217976d1f69d8e92dcda6708e424bfc
mosersil <silviomos@gmail.com>
2019-03-18 20:37:04 +0100

    use ehcache

---
+++ src/main/java/com/silviomoser/demo/services/CacheManagerService.java
@@ -0,0 +1,19 @@
+package com.silviomoser.demo.services;
+
+import lombok.extern.slf4j.Slf4j;
+import net.sf.ehcache.CacheManager;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class CacheManagerService {
+    private CacheManager cacheManager = CacheManager.getInstance();
+
+    public void clearCache() {
+        cacheManager.clearAll();
+    }
+
+    public void clearCache(String cacheName) {
+        cacheManager.getCache(cacheName).flush();
+    }
+}
--- src/main/java/com/silviomoser/demo/services/MembershipService.java
+++ src/main/java/com/silviomoser/demo/services/MembershipService.java
@@ -7,6 +7,7 @@ import com.silviomoser.demo.repository.OrganizationRepository;
 import com.silviomoser.demo.utils.FormatUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,12 +17,18 @@ import java.util.stream.Collectors;
 @Slf4j
 public class MembershipService {
 
-    @Autowired
-    MembershipRepository membershipRepository;
+    private static final String CACHE_MEMBERLIST = "memberlist";
 
     @Autowired
-    OrganizationRepository organizationRepository;
+    private MembershipRepository membershipRepository;
 
+    @Autowired
+    private OrganizationRepository organizationRepository;
+
+    @Autowired
+    private CacheManagerService cacheManagerService;
+
+    @Cacheable(CACHE_MEMBERLIST)
     public List<Membership> getMemberList(String organizationName) {
         List<Membership> result = null;
         final Organization organization = organizationRepository.findByName(organizationName);
@@ -35,11 +42,13 @@ public class MembershipService {
 
     public void delete(Membership membership) throws ServiceException {
         membershipRepository.delete(membership);
+        cacheManagerService.clearCache(CACHE_MEMBERLIST);
         log.info("Deleted membership {}", FormatUtils.toFirstLastName(membership.getPerson()));
     }
 
     public Membership addOrUpdate(Membership membership) throws ServiceException {
         Membership membership1 = membershipRepository.save(membership);
+        cacheManagerService.clearCache(CACHE_MEMBERLIST);
         log.info("Saved membership {}", FormatUtils.toFirstLastName(membership.getPerson()));
         return membership1;
     }
