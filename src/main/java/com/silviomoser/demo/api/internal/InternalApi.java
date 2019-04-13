7ad57d3d2c457731d655ac5b598b9a347eb8dcad
mosersil <silviomos@gmail.com>
2019-04-13 18:44:48 +0200

    updated PDF report generator

--- src/main/java/com/silviomoser/demo/api/internal/InternalApi.java
+++ src/main/java/com/silviomoser/demo/api/internal/InternalApi.java
@@ -74,7 +74,9 @@ public class InternalApi implements ApiController {
                                        @RequestParam(name = "format") AddressListFormat format) {
         try {
             log.debug("Assemble document {} in format {}", organization, format);
-            return new ModelAndView(format.name(), "entries", addresslistService.generateAddressList(organization));
+            ModelAndView modelAndView =  new ModelAndView(format.name(), "entries", addresslistService.generateAddressList(organization));
+            modelAndView.addObject("title", "Adressliste " + organization);
+            return modelAndView;
         } catch (Exception e) {
             log.error(e.getMessage(), e);
             throw new ApiException(e.getMessage());
