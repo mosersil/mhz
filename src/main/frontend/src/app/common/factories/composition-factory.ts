feac005bd6bb4c80d5d87af89feea9aa61735b36
mosersil <silviomos@gmail.com>
2020-02-09 21:54:44 +0100

    further development

---
+++ src/main/frontend/src/app/common/factories/composition-factory.ts
@@ -0,0 +1,21 @@
+import {Composition} from "../entities/composition";
+
+export class CompositionFactory {
+
+
+  static empty(): Composition {
+    return {
+      id: 0,
+      inventory: '',
+      title: '',
+      subtitle: '',
+      description: '',
+      genre: '',
+      tag: '',
+      composers: [ {id: 0, name: ''} ],
+      arrangers: [ {id: 0, name: ''} ],
+      sheets: [ {id: 0, title: '', location: ''} ],
+      samples: [ {id: 0, title: '', location: ''} ]
+    };
+  }
+}
